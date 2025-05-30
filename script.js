dfa7e73e5c4e127a5585273a8afe017f82d2a1ac
Easily <easily.plays@gmail.com>
2025-05-31 01:47:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const pseudo = "Easiiily";
+
+document.getElementById("pseudo").textContent = pseudo;
+document.getElementById("intro-text").textContent = 
+  `Salut, moi c'est ${pseudo} ! Passionné de gaming, je stream sur Twitch, je crée du contenu sur YouTube et je suis très actif sur TikTok, Instagram et X. Rejoins-moi pour ne rien rater !`;
+
